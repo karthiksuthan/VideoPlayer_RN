566b5ddf42b6436be9cd3de36f3f7a53125b140c
Karthik Suthan <karthik.s@zerone-consulting.com>
2022-03-23 11:53:00 +0530

    commit 1

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppContainer from './src/AppContainer';
+
+const App: React.FC = () => {
+  return (
+        <AppContainer />
+  );
+};
+
+export default App;
---
+++ Assets/Image.tsx
@@ -0,0 +1,9 @@
+const Images = {
+  play: require('./images/play.png'),
+  sound: require('./images/sound.png'),
+  mute: require('./images/mute.png'),
+  pause: require('./images/pause.png'),
+  subtitle: require('./images/subtitles.png'),
+};
+
+export default Images
